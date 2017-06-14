a84ab11003dd77c6a3850fe0a188c46d6ae73117
eiva <me@eiva.info>
2017-06-13 22:39:42 -0400

    Fix typo

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-docker login -r $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
