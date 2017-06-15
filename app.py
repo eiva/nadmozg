82ce6452c93d013008e52491a2e82504334e43ae
Eugene Ivanchenko <astellar@live.com>
2017-06-15 10:30:24 -0400

    Try to add color border to weather messages

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import requests
 import os
-import discord
 import asyncio
 import sys
 import psycopg2
 import urllib.parse
-from discord.ext.commands import Bot
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+import discord
+from discord.ext.commands import Bot
+from discord.embeds import Embed
+
 import socket
 from contextlib import contextmanager
 import time
@@ -113,23 +116,24 @@ def reload(*args):
     yield from my_bot.say("Reloading...")
     yield from my_bot.close()
 
-
-@my_bot.command()
-@asyncio.coroutine
-def weather(*param):
+def color_by_temp(t):
     '''
-    Show weather forecast for location.
+    Select color from temperature in C.
     '''
-    print(param)
-    if len(param) != 1:
-        yield from my_bot.say('Ussage: !weather {dc, mos, b, nyc, sf}')
-        return
+    if t < -15:
+        return 0
+    elif t < 0:
+        return 0x1433ce
+    elif t < 15:
+        return 0x83eac9
+    elif t < 25:
+        return 0x45d140
+    elif t < 30:
+        return 0xd6cd20
+    else:
+       return 0xd64420
 
-    loc_name = param[0].upper()
-    loc = session.query(GeoLoc).filter(GeoLoc.name == loc_name).first()
-    if not loc:
-         yield from my_bot.say('Use `!config geo list` to see all locations or `!config geo add` to add new one')
-
+def get_forecast(loc: GeoLoc):
     token = os.environ['DARKSKY_TOKEN']
     url = 'https://{url}/{token}/{latitude:.4f},{longitude:.4f}'.format(
         url='api.darksky.net/forecast',
@@ -141,19 +145,37 @@ def weather(*param):
     async_request = loop.run_in_executor(None, requests.get, url)
     response = yield from async_request
 
-    data = response.json()['currently']
+    return response.json()['currently']
+
+@my_bot.command(pass_context=True)
+async def weather(ctx, *param):
+    '''
+    Show weather forecast for location.
+    '''
+    print(param)
+    if len(param) != 1:
+        await my_bot.say('Ussage: !weather {dc, mos, b, nyc, sf}')
+        return
+
+    loc_name = param[0].upper()
+    loc = session.query(GeoLoc).filter(GeoLoc.name == loc_name).first()
+    if not loc:
+         await my_bot.say('Use `!config geo list` to see all locations or `!config geo add` to add new one')
+
+    data = get_forecast(loc)
 
     def to_celsius(f):
         return (float(f) - 32.0) * 5.0 / 9.0
-
-    result = '{0:.0f} \u00B0C'.format(to_celsius(data['apparentTemperature']))
+    t_in_c = to_celsius(data['apparentTemperature'])
+    result = '{0:.0f} \u00B0C'.format(t_in_c)
     if data['windSpeed'] >= 2.0:
         result += ' at {0:.1f} mph wind'.format(data['windSpeed'])
     if data['precipProbability'] > 0:
         result += ' and I am {0:.0f}% sure it is {1}'.format(
             data['precipProbability'] * 100,
             'raining' if data['temperature'] > 32.0 else 'snowing')
-    yield from my_bot.say(result)
+    embed = Embed(colour=color_by_temp(t_in_c), title="Weather in " + loc_name, description=result)
+    await my_bot.send_message(ctx.message.channel, content = None, embed=embed)
 
 @my_bot.command()
 @asyncio.coroutine
